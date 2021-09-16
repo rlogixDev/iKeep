43e6de0d4806db7a5ffdf6a95b0f170120f47bc8
Adarsh Pandey <adarshpandey022@gmail.com>
2021-09-16 18:30:09 +0530

    validation done

--- src/App.js
+++ src/App.js
@@ -2,25 +2,18 @@ import React from 'react';
 import './App.css';
 import Login from './screens/Login/login';
 import Header from './components/header/header';
-import { Route, Switch} from 'react-router-dom';
-import HomePage from './screens/Homepage/homepage'
+import { Route, Switch } from 'react-router-dom';
+import HomePage from './screens/Homepage/homepage';
 import Signup from './screens/Signup/signup';
 export default function App() {
   return (
-  
-   
-   
-    <div className="App">
-      <Header/>
+    <div className='App'>
+      <Header />
       <Switch>
-        <Route exact  path="/" component={Login} />
-        <Route exact  path="/signup" component={Signup} />
-        <Route exact  path="/homepage" component={HomePage} />
+        <Route exact path='/' component={Login} />
+        <Route exact path='/signup' component={Signup} />
+        <Route exact path='/homepage' component={HomePage} />
       </Switch>
     </div>
-  
-  
-   
- 
   );
 }
--- src/screens/Signup/signup.js
+++ src/screens/Signup/signup.js
@@ -1,115 +1,321 @@
-import React,{useState} from 'react';
+import React, { useState, useEffect } from 'react';
 import Form from 'react-bootstrap/Form';
-import { Row,Container,Col } from 'react-bootstrap';
+import { Row, Container, Col } from 'react-bootstrap';
 import Button from 'react-bootstrap/Button';
 import { Link } from 'react-router-dom';
 import './signup.css';
-export default function Signup(){
 
-    const [name,setName] =useState('');
-    const [phone,setPhone]=useState('');
-    const [email,setEmail] =useState('');
+export default function Signup() {
+  const [name, setName] = useState('');
+  const [password, setPassword] = useState('');
+  const [phone, setPhone] = useState('');
+  const [gender, setGender] = useState('');
+  const [zip, setZip] = useState('');
+  const [state, setState] = useState('');
+  const [country, setCountry] = useState('');
+  const [email, setEmail] = useState('');
 
-    return (<>
-   
-    <div className="form mt-4">
+  const validEmail = new RegExp('@mediaagility.com$');
 
-    <Form className="contain mx-auto p-4 ">
-    <h3 className="mb-3">SignUp</h3>
-    <Container>
-    <Form.Group className="mb-3">
-   
-    <Row className="justify-content-center">
-  <Form.Label column sm="4">Username</Form.Label>
-    <Col  sm="8"><Form.Control  type="text" placeholder="Enter Username" /></Col>
-    </Row>
-   
-  </Form.Group>
- 
-  <Form.Group className="mb-3">
- 
-      <Row className="justify-content-center">
-  <Col  sm="4"> <Form.Label>Password</Form.Label></Col>
-   <Col sm="8"> <Form.Control type="password" placeholder="Enter Password" /></Col>
-    </Row>
-   
-  </Form.Group>
-  <Form.Group className="mb-3">
- 
-  <Row className="justify-content-center">
-   <Col  sm="4"> <Form.Label>Mobile Number</Form.Label></Col>
-   <Col sm="8"> <Form.Control type="text" placeholder="Enter Mobile Number" /></Col>
-    </Row>
-  
-  </Form.Group>
-  <Form.Group className="mb-3">
- 
- <Row className="justify-content-center">
-  <Col  sm="4"> <Form.Label>E-mail</Form.Label></Col>
-  <Col sm="8"> <Form.Control type="text" placeholder="Enter E-mail" /></Col>
-   </Row>
- 
- </Form.Group>
-  <Form.Group className="mb-3">
- 
-  <Row className="justify-content-center">
-  <Col  sm="4"> <Form.Label>Gender</Form.Label></Col>
-   <Col sm="2" > <Form.Check type="radio" name="Gender" label="M" /></Col>
-   <Col sm="3"> <Form.Check type="radio" name="Gender" label="F" /></Col>
-   <Col sm="3">  <Form.Check type="radio" name="Gender" label="Others" /></Col>
-  
-    </Row>
-   
-  </Form.Group>
-  <Form.Group className="mb-3">
- 
-  <Row className="justify-content-center">
-   <Col sm="4"> <Form.Label>Zip Code</Form.Label></Col>
-   <Col sm="8"> <Form.Control type="text" placeholder="Enter Zip Code" /></Col>
-    </Row>
-   
-  </Form.Group>
-  <Form.Group className="mb-3">
- 
-   
-  <Row className="justify-content-center">
-  <Col sm="4"> <Form.Label>State</Form.Label></Col>
-  <Col sm="8"> <Form.Control type="text" placeholder="Enter State" /></Col>
-   </Row>
-    
-   
-  </Form.Group>
-  <Form.Group className="mb-3">
- <Row className="justify-content-center">
-  <Col sm="4"> <Form.Label>Country</Form.Label></Col>
-  <Col sm="8"> <Form.Control type="text" placeholder="Enter Country" /></Col>
-   </Row>
- </Form.Group>
+  const validPassword = new RegExp(
+    '^[a-zA-Z0-9._:$!%-]+@[a-zA-Z0-9.-]+.[a-zA-Z]$'
+  );
 
+  function phoneNumberCheck(e) {
+    const re = /^[0-9\b]+$/;
+    if (e.target.value === '' || re.test(e.target.value)) {
+      if (e.target.value.length < 11) {
+        setPhone(e.target.value);
+      }
+    }
+  }
 
- <Form.Group className="mb-3">
- 
- <Row className="justify-content-start ">
- <Col sm="4" ></Col>
- <Col sm={8} className="d-flex justify-content-start"><Button type="submit" className="mb-3" >Register</Button></Col>
-    </Row>
-  </Form.Group>
- 
- 
-  <Row>
- 
-  
-  </Row>
-  <p className="link">Already have an account? <Link to="/">Login</Link></p>
-</Container>
-    </Form>
-   
-    </div>
-    
-    </>)
+  function zipCodeCheck(e) {
+    const re = /^[0-9\b]+$/;
+    if (e.target.value === '' || re.test(e.target.value)) {
+      if (e.target.value.length < 7) {
+        setZip(e.target.value);
+      }
+    }
+  }
 
-
-
-
-
+  return (
+    <>
+      <div className='form mt-4'>
+        <Form className='contain mx-auto p-4 '>
+          <h3 className='mb-3'>SignUp</h3>
+          <Container>
+            <Form.Group className='mb-3'>
+              <Row className='justify-content-center'>
+                <Form.Label column sm='4'>
+                  Username
+                </Form.Label>
+                <Col sm='8' className='p-0'>
+                  <Form.Control
+                    type='text'
+                    placeholder='Enter Username'
+                    onChange={(e) => setName(e.target.value)}
+                  />
+                </Col>
+              </Row>
+            </Form.Group>
+            <Form.Group className='mb-3'>
+              <Row className='justify-content-center'>
+                <Col sm='4'>
+                  {' '}
+                  <Form.Label>Password</Form.Label>
+                </Col>
+                <Col sm='8'>
+                  {' '}
+                  <Row>
+                    <Form.Control
+                      type='password'
+                      placeholder='Enter Password'
+                      onChange={(e) => setPassword(e.target.value)}
+                    />
+                  </Row>
+                  <Row>
+                    {!validPassword.test(password) && (
+                      <p
+                        style={{
+                          textAlign: 'left',
+                          color: 'red',
+                          fontSize: '10px',
+                          marginBottom: '5px',
+                        }}
+                      >
+                        *Minimum eight characters, at least one uppercase
+                        letter, one lowercase letter, one number and one special
+                        character
+                      </p>
+                    )}
+                  </Row>
+                </Col>
+              </Row>
+            </Form.Group>
+            <Form.Group className='mb-3'>
+              <Row className='justify-content-center'>
+                <Col sm='4'>
+                  <Form.Label>Mobile Number</Form.Label>
+                </Col>
+                <Col sm='8'>
+                  {' '}
+                  <Row>
+                    <Form.Control
+                      type='text'
+                      placeholder='Enter Mobile Number'
+                      value={phone}
+                      onChange={phoneNumberCheck}
+                    />
+                  </Row>
+                  <Row>
+                    <Row>
+                      {phone.length != 10 && (
+                        <p
+                          style={{
+                            textAlign: 'left',
+                            color: 'red',
+                            fontSize: '10px',
+                            marginBottom: '5px',
+                          }}
+                        >
+                          *Please enter 10 digit Mobile Number
+                        </p>
+                      )}
+                    </Row>
+                  </Row>
+                </Col>
+              </Row>
+            </Form.Group>
+            <Form.Group className='mb-3'>
+              <Row className='justify-content-center'>
+                <Col sm='4'>
+                  {' '}
+                  <Form.Label>E-mail</Form.Label>
+                </Col>
+                <Col sm='8'>
+                  <Row>
+                    <Form.Control
+                      type='text'
+                      placeholder='Enter E-mail'
+                      onChange={(e) => setEmail(e.target.value)}
+                    />
+                  </Row>
+                  <Row>
+                    {!validEmail.test(email) && (
+                      <p
+                        style={{
+                          textAlign: 'left',
+                          color: 'red',
+                          fontSize: '10px',
+                          marginBottom: '5px',
+                        }}
+                      >
+                        *should end with @mediaagility.com
+                      </p>
+                    )}
+                  </Row>
+                </Col>
+              </Row>
+            </Form.Group>
+            <Form.Group className='mb-3'>
+              <Row className='justify-content-center'>
+                <Col sm='4'>
+                  <Form.Label>Gender</Form.Label>
+                </Col>
+                <Col sm='2'>
+                  <Form.Check
+                    type='radio'
+                    name='Gender'
+                    label='M'
+                    value='male'
+                    onClick={(e) => setGender(e.target.value)}
+                  />
+                </Col>
+                <Col sm='3'>
+                  <Form.Check
+                    type='radio'
+                    name='Gender'
+                    label='F'
+                    value='female'
+                    onClick={(e) => setGender(e.target.value)}
+                  />
+                </Col>
+                <Col sm='3'>
+                  <Form.Check
+                    type='radio'
+                    name='Gender'
+                    label='Others'
+                    value='others'
+                    onClick={(e) => setGender(e.target.value)}
+                  />
+                </Col>
+                {gender.length == 0 && (
+                  <p
+                    style={{
+                      width: '100px',
+                      color: 'red',
+                      fontSize: '10px',
+                      marginBottom: '5px',
+                    }}
+                  >
+                    *Enter Gender
+                  </p>
+                )}
+              </Row>
+            </Form.Group>
+            <Form.Group className='mb-3'>
+              <Row className='justify-content-center'>
+                <Col sm='4'>
+                  {' '}
+                  <Form.Label>Zip Code</Form.Label>
+                </Col>
+                <Col sm='8'>
+                  <Row>
+                    <Form.Control
+                      type='text'
+                      placeholder='Enter Zip Code'
+                      value={zip}
+                      onChange={zipCodeCheck}
+                    />
+                  </Row>
+                  <Row>
+                    {zip.length != 6 && (
+                      <p
+                        style={{
+                          textAlign: 'left',
+                          color: 'red',
+                          fontSize: '10px',
+                          marginBottom: '5px',
+                        }}
+                      >
+                        *Enter ZipCode
+                      </p>
+                    )}
+                  </Row>
+                </Col>
+              </Row>
+            </Form.Group>
+            <Form.Group className='mb-3'>
+              <Row className='justify-content-center'>
+                <Col sm='4'>
+                  {' '}
+                  <Form.Label>State</Form.Label>
+                </Col>
+                <Col sm='8'>
+                  {' '}
+                  <Row>
+                    <Form.Control
+                      type='text'
+                      placeholder='Enter State'
+                      onClick={(e) => setState(e.target.value)}
+                    />
+                  </Row>
+                  <Row>
+                    {state.length == 0 && (
+                      <p
+                        style={{
+                          textAlign: 'left',
+                          color: 'red',
+                          fontSize: '10px',
+                          marginBottom: '5px',
+                        }}
+                      >
+                        *Enter State
+                      </p>
+                    )}
+                  </Row>
+                </Col>
+              </Row>
+            </Form.Group>
+            <Form.Group className='mb-3'>
+              <Row className='justify-content-center'>
+                <Col sm='4'>
+                  {' '}
+                  <Form.Label>Country</Form.Label>
+                </Col>
+                <Col sm='8'>
+                  <Row>
+                    <Form.Control
+                      type='text'
+                      placeholder='Enter Country'
+                      onClick={(e) => setCountry(e.target.value)}
+                    />
+                  </Row>
+                  <Row>
+                    {country.length == 0 && (
+                      <p
+                        style={{
+                          textAlign: 'left',
+                          color: 'red',
+                          fontSize: '10px',
+                          marginBottom: '5px',
+                        }}
+                      >
+                        *Enter Country
+                      </p>
+                    )}
+                  </Row>
+                </Col>
+              </Row>
+            </Form.Group>
+            <Form.Group className='mb-3'>
+              <Row className='justify-content-start '>
+                <Col sm='4'></Col>
+                <Col sm={8} className='d-flex justify-content-start'>
+                  <Button type='submit' className='mb-3'>
+                    Register
+                  </Button>
+                </Col>
+              </Row>
+            </Form.Group>
+            <Row></Row>
+            <p className='link'>
+              Already have an account? <Link to='/'>Login</Link>
+            </p>
+          </Container>
+        </Form>
+      </div>
+    </>
+  );
 }
