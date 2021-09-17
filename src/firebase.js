7de711978397bdf24b8ff668667114c5d00fd0be
vedikamujgule <vedikamujgule@gmail.com>
2021-09-17 14:49:27 +0530

    [update] login firebase auth

--- src/firebase.js
+++ src/firebase.js
@@ -1,15 +1,30 @@
-import * as firebase from "firebase/app";
-import "firebase/auth";
+import { initializeApp } from 'firebase/app';
+// import { getAuth,getFirestore, createUserWithEmailAndPassword } from "firebase/auth";
 
-const app = firebase.initializeApp({
+// TODO: Replace the following with your app's Firebase project configuration
+const firebaseConfig = {
     apiKey: "AIzaSyBJZ5X8lgrNErxR5rxeTW8QNOcJTPh4DaY",
     authDomain: "ikeep-authentication.firebaseapp.com",
     projectId: "ikeep-authentication",
     storageBucket: "ikeep-authentication.appspot.com",
     messagingSenderId: "832795327299",
     appId: "1:832795327299:web:9de52e4d31df87690e4df6"
-});
-console.log("firebase", app)
+};
 
-// export const auth = app.auth();
-export default app
+const authApp = initializeApp(firebaseConfig);
+export default authApp;
+
+// const auth = getAuth(app);
+// const db = getFirestore(app);
+
+// createUserWithEmailAndPassword(auth, email, password)
+//   .then((userCredential) => {
+//     // Signed in 
+//     const user = userCredential.user;
+//     // ...
+//   })
+//   .catch((error) => {
+//     const errorCode = error.code;
+//     const errorMessage = error.message;
+//     // ..
+//   });
--- src/screens/Login/login.js
+++ src/screens/Login/login.js
@@ -1,27 +1,60 @@
 
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react'
 import { Link } from 'react-router-dom';
-import { useAuth } from "../../context/AuthContext";
 import './login.css';
+import { getAuth, signInWithEmailAndPassword } from "firebase/auth";
+import authApp from '../../firebase';
+import { signInWithPopup, GoogleAuthProvider } from "firebase/auth";
 
 export default function Login() {
     const [username, setUserName] = useState();
     const [password, setPassword] = useState();
-    const  {login}  = useAuth()
-    const [error, setError] = useState("")
-    const [loading, setLoading] = useState(false)
-
-    async function handleSubmit(e) {      
-        try {
-          setError("")
-          setLoading(true)
-          await login(username,password);
-        } catch {
-          setError("Failed to log in")
-          console.log('error in login', error)
-        }
-        setLoading(false)
+    const [currentUser, setCurrentUser] = useState()
+    // const  {login}  = useAuth()
+    const [error, setError] = useState()
+    const [disabled, disableButton] = useState(true)
+    const auth = getAuth(authApp);
+    
+    async function handleSubmit() {
+        await signInWithEmailAndPassword(auth, username, password)
+            .then((userCredential) => {
+                const user = userCredential.user;
+                setCurrentUser(user);
+                console.log("user logged in via email/password", currentUser.auth.currentUser.email)
+            })
+            .catch((error) => {
+                const errorMessage = error.code;
+                setError('**'+errorMessage.trim().substring(5))
+            });
     }
+    async function handleGoogleSignIn() {
+        const provider = new GoogleAuthProvider();
+        await signInWithPopup(auth, provider)
+            .then((result) => {
+                // This gives you a Google Access Token. You can use it to access the Google API.
+                const credential = GoogleAuthProvider.credentialFromResult(result);
+                const token = credential.accessToken;
+                // The signed-in user info.
+                const user = result.user;
+                console.log(user)
+                // ...
+            }).catch((error) => {
+                // Handle Errors here.
+                const errorCode = error.code;
+                const errorMessage = error.message;
+                // The email of the user's account used.
+                const email = error.email;
+                // The AuthCredential type that was used.
+                const credential = GoogleAuthProvider.credentialFromError(error);
+                // ...
+            });
+    }
+
+    useEffect(() => {
+        auth.onAuthStateChanged(user => {
+            setCurrentUser(user);
+        })
+    }, [])
 
     const ColoredLine = ({ color }) => (
         <hr
@@ -46,8 +79,8 @@ export default function Login() {
                         </div>
                         <div className="col-8">
                             <div className="form-group p-2 ">
-                                <input type="email" className="form-control" name="username"  placeholder="Enter email" onChange={e => setUserName(e.target.value)} />
-                                {!username&& <p className="error">*Enter email</p>}
+                                <input type="email" className="form-control" name="username" placeholder="Enter email" onChange={e => setUserName(e.target.value)} />
+                                {!username && <p className="error">*Enter email</p>}
                             </div>
                         </div>
                     </div>
@@ -59,21 +92,27 @@ export default function Login() {
                         </div>
                         <div className="col-8">
                             <div className="form-group p-2">
-                                <input type="password" className="form-control" name="password"  placeholder="Enter password" onChange={e => setPassword(e.target.value)} />
+                                <input type="password" className="form-control" name="password" placeholder="Enter password" onChange={e => setPassword(e.target.value)} />
                                 {!password && <p className="error">*Enter password</p>}
                             </div>
                         </div>
                     </div>
-                    <div id="login-buttons" className="row justify-content-end ">
+                    <div id="login-buttons" className="row justify-content-center ">
                         <div >
-                            <button variant="primary" type="button" className="btn btn-primary btn-lg btn-block" onClick={()=>handleSubmit()} disabled={loading}>Submit</button>
+                            {/* <Link to="/homepage"> */}
+                            <button variant="primary" type="button" className="btn btn-primary btn-lg btn-block" onClick={() => handleSubmit()} disabled={!username | !password}> Submit</button>
+                           {/* </Link> */}
                         </div>
+                        <p className="error">{error}
+                        </p>
+                        <p className="link">Forgot Password? <Link to="/resetPassword">Reset Password</Link></p>
+
                     </div>
 
                     <div className="row ">
                         <ColoredLine color="red" />
                         <div >
-                            <button variant="primary" type="submit" className="btn btn-light btn-md pr-10"> <img src="https://e7.pngegg.com/pngimages/114/607/png-clipart-g-suite-pearl-river-middle-school-google-software-suite-email-sign-up-button-text-logo-thumbnail.png" width="50" radius="10" alt="google" />
+                            <button variant="primary" type="submit" className="btn btn-light btn-md pr-10" onClick={() => handleGoogleSignIn()} > <img src="https://e7.pngegg.com/pngimages/114/607/png-clipart-g-suite-pearl-river-middle-school-google-software-suite-email-sign-up-button-text-logo-thumbnail.png" width="50" radius="10" alt="google" />
                                 Sign in With Google</button>
                         </div>
                         <p className="link">New to IKeep? <Link to="/signup">Join now</Link></p>
