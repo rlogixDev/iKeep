b9a10f7417bcc1edc72f03927c1f88ff7fa3554c
Shikhar Goel <shikhar.goel@mediaagility.com>
2021-09-24 00:05:43 +0530

    moved the search at top

--- src/components/homepageComponent/InputNote.js
+++ src/components/homepageComponent/InputNote.js
@@ -1,5 +1,5 @@
 import React, { useState,useContext} from 'react';
-import { Form, Card, Button, InputGroup, FormControl } from 'react-bootstrap';
+import { Form, Card, Button, InputGroup, FormControl,Container,Dropdown,Row,ButtonGroup} from 'react-bootstrap';
 import { RiAddLine } from 'react-icons/ri';
 import { Image, CloudinaryContext } from 'cloudinary-react';
 import axios from 'axios';
@@ -9,7 +9,7 @@ import NotesDisplay from './NotesDisplay';
 
 
 export default function InputNote() {
-  
+  const [searchText, setSearch] = useState();
   const[newNote,setNewNote] =useState({});
   const {activeUser} = useContext(AuthContext);
   console.log(activeUser.uid);
@@ -56,6 +56,44 @@ export default function InputNote() {
 
   return (
     <>
+    <Container>
+    <Row>
+          {/* <SearchBox searchText={searchText}/> */}
+          <div className='d-flex justify-content-center'>
+            <Form
+              className='d-flex justify-content-around p-0 mb-5'
+              style={{
+                width: '50rem',
+              }}
+            >
+              <FormControl
+                type='search'
+                placeholder='Search'
+                aria-label='Search '
+                aria-describedby='basic-addon2'
+                onChange={e => setSearch(e.target.value)}
+                style={{ maxWidth: '500px' }}
+              />
+              <Dropdown style={{ margin: '0px 10px' }} as={ButtonGroup}>
+                <Button variant='outline-info'>Sort</Button>
+
+                <Dropdown.Toggle
+                  split
+                  variant='outline-info'
+                  id='dropdown-split-basic'
+                />
+
+                <Dropdown.Menu>
+                  <Dropdown.Item href='#/action-1'>Date Wise</Dropdown.Item>
+                  <Dropdown.Item href='#/action-2'>Alphabetical Wise</Dropdown.Item>
+                </Dropdown.Menu>
+              </Dropdown>
+              <Button variant='outline-success'>Save</Button>{' '}
+            </Form>
+          </div>
+        
+        </Row>
+        </Container>
       <Card
         className='position-relative top-0 start-50 translate-middle-x  '
         style={{
--- src/components/homepageComponent/NotesDisplay.js
+++ src/components/homepageComponent/NotesDisplay.js
@@ -49,42 +49,6 @@ export default function NotesDisplay({note}) {
   return (
     <>
       <Container>
-        <Row>
-          {/* <SearchBox searchText={searchText}/> */}
-          <div className='d-flex justify-content-center'>
-            <Form
-              className='d-flex justify-content-around p-0 mb-5'
-              style={{
-                width: '50rem',
-              }}
-            >
-              <FormControl
-                type='search'
-                placeholder='Search'
-                aria-label='Search '
-                aria-describedby='basic-addon2'
-                onChange={e => setSearch(e.target.value)}
-                style={{ maxWidth: '500px' }}
-              />
-              <Dropdown style={{ margin: '0px 10px' }} as={ButtonGroup}>
-                <Button variant='outline-info'>Sort</Button>
-
-                <Dropdown.Toggle
-                  split
-                  variant='outline-info'
-                  id='dropdown-split-basic'
-                />
-
-                <Dropdown.Menu>
-                  <Dropdown.Item href='#/action-1'>Date Wise</Dropdown.Item>
-                  <Dropdown.Item href='#/action-2'>Alphabetical Wise</Dropdown.Item>
-                </Dropdown.Menu>
-              </Dropdown>
-              <Button variant='outline-success'>Save</Button>{' '}
-            </Form>
-          </div>
-        
-        </Row>
         <Row className='d-flex flex-row justify-content-center mt-3'>
           <Button
             variant='primary'
