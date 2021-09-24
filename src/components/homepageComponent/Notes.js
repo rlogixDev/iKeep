32ec4964e53e56a1209ebfa212363f7719b04811
Shikhar Goel <shikhar.goel@mediaagility.com>
2021-09-24 22:15:00 +0530

    multiple api call issue

--- src/components/homepageComponent/Notes.js
+++ src/components/homepageComponent/Notes.js
@@ -3,19 +3,19 @@ import { Card } from 'react-bootstrap';
 
 export default function Notes({data}) {
   console.log("Notes data",data);
-  const date=data[0].Date;
+ 
   var days = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
   const monthNames = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December" ];
-  const extractedDate=date.substr(4,6)
-  console.log("date",date);
-  const today=data.filter((item,key) =>  (item.Date  === Date(Date.now).toString().substr(0,15) ) )
+ 
+ 
+  const today=data.filter((item,key) =>  (item? item.Date  === Date(Date.now).toString().substr(0,15):'' ) )
 
   const yesDay = days[new Date().getDay()-1].substr(0,3);
   const yesMon=monthNames[new Date().getMonth()].substr(0,3);
   const yesYear=new Date().getFullYear();
   const yesDate=new Date().getDate()-1;
   const yesterday=yesDay + " " + yesMon + " " + yesDate + " " + yesYear;
-  const yesterdayData=data.filter((item,key) =>  (item.Date  === yesterday ) )
+  const yesterdayData=data.filter((item,key) =>  (item? item.Date  === yesterday :'' ) )
   console.log("yesterday",yesterday);
   return (
     
@@ -23,8 +23,8 @@ export default function Notes({data}) {
       <h4 className='text-decoration-underline' style={{ textAlign: 'left' }}>
         Today
       </h4>
-      {today.map((data, index) => (
-        <>
+      {today.map((data, index) => (data?
+        (<>
           <Card  style={{ width: '18rem', borderRadius: '15px' }} className='m-2'>
             <Card.Body>
               <input
@@ -37,13 +37,13 @@ export default function Notes({data}) {
               <Card.Link href='#'>Edit</Card.Link>
             </Card.Body>
           </Card>
-        </>
+        </>):''
       ))}
       <h4 className='text-decoration-underline' style={{ textAlign: 'left' }}>
         Yesterday
       </h4>
-      {yesterdayData.map((data, index) => (
-        <>
+      {yesterdayData.map((data, index) => (data?
+        (<>
           <Card  style={{ width: '18rem', borderRadius: '15px' }} className='m-2'>
             <Card.Body>
               <input
@@ -56,7 +56,7 @@ export default function Notes({data}) {
               <Card.Link href='#'>Edit</Card.Link>
             </Card.Body>
           </Card>
-        </>
+        </>):''
       ))}
     </div>
   );
--- src/components/homepageComponent/NotesDisplay.js
+++ src/components/homepageComponent/NotesDisplay.js
@@ -1,9 +1,11 @@
 import React, { useState, useEffect, useContext } from "react";
-import InputNote from "./InputNote";
+
 import Notes from "./Notes";
-import { Row, Container, Button } from "react-bootstrap";
+import { RiAddLine } from 'react-icons/ri';
+import { Row, Container, Button ,Card,InputGroup} from "react-bootstrap";
+import { Image, CloudinaryContext } from 'cloudinary-react';
+import { getDatabase,ref, set } from "firebase/database";
 import axios from "axios";
-import SearchBox from "./SearchBox";
 import { AuthContext } from "../../context/AuthContext";
 
 import { Form, FormControl, Dropdown, ButtonGroup } from "react-bootstrap";
@@ -14,35 +16,81 @@ export default function NotesDisplay() {
   // const [userNotes,setUserNotes] =useState([]);
   const { activeUser } = useContext(AuthContext);
   const uid = activeUser.uid;
+  const [title,setTitle] =useState('');
+  const [Content,setContent] =useState('');   
+  const [newNote,setNewNote]=useState({});  
+  
+  const [addImg, setAddImg] = useState('');
+
+       
   console.log("uid", uid);
+  const AddNote =() => {
+ 
+    const db = getDatabase();
+    const id =Math.round(Math.random()*100);
+    set(ref(db,'notes/'+activeUser.uid+'/'+id), {
+      id:id,
+      title: title,
+      Content: Content,
+      Email:activeUser.email,
+      Date:Date(Date.now).toString().substr(0,15)
+    }).
+    then(() => console.log("Added successfully"),
+    setNewNote({"id":id,"title":title,"Content":Content,"Email":activeUser,"Date":Date(Date.now).toString().substr(0,15)}),
+    console.log("newNote",newNote)
+    ).
+    catch(() => console.log("Error"));
+  }
+
+  const uploadImage = async () => {
+    const formData = new FormData();
+    formData.append('file', addImg);
+    formData.append('upload_preset', 'lmx0ng0b');
+    await axios
+      .post('https://api.cloudinary.com/v1_1/adarsh022/image/upload', {
+        method: 'POST',
+        body: formData,
+      })
+      .then((res) => {
+        console.log('res', res);
+      });
+  };
+
+  console.log(addImg);
   useEffect(() => {
-    axios
+ axios
+      .get("https://react-project-1443c-default-rtdb.firebaseio.com/notes.json")
+      .then((res) => { (setData(res.data))});
+  }, [newNote]);
+  useEffect(() => {
+ axios
       .get("https://react-project-1443c-default-rtdb.firebaseio.com/notes.json")
       .then((res) => setData(res.data));
-  }, [data]);
+  }, []);
+
+  console.log("data",data);
   let userNotes = [];
   let a = data[uid]
-    ? (Object.keys(data[uid]).map((item) => userNotes.push(data[uid][item])),
-      console.log(userNotes))
-    : "";
+    ? (Object.keys(data[uid]).map((item) => userNotes.push(data[uid][item]))): '';
+    console.log("userNotes",userNotes);
+    
 
-  const [filteredData, setFilteredData] = useState(data);
+    const [filteredData, setFilteredData] = useState(userNotes);
   console.log(searchText);
 
   useEffect(() => {
     const lowercasedValue = searchText?.toLowerCase().trim();
-    if (lowercasedValue === "") userNotes(userNotes);
+    if (lowercasedValue === "") setFilteredData(userNotes);
     else {
       const filteredData = userNotes.filter(
-        (item) =>
-          item.title.toLowerCase().includes(lowercasedValue) ||
-          item.Content.toLowerCase().includes(lowercasedValue)
+        (item) =>item?
+         ((item.title.includes(searchText))):''
       );
-      // console.log("filteredData", filteredData);
+      console.log("filteredData", filteredData);
       setFilteredData(filteredData);
     }
   }, [searchText]);
-
+ 
   return (
     <>
       <Container>
@@ -63,6 +111,10 @@ export default function NotesDisplay() {
                 onChange={(e) => setSearch(e.target.value)}
                 style={{ maxWidth: "500px" }}
               />
+
+
+
+
               <Dropdown style={{ margin: "0px 10px" }} as={ButtonGroup}>
                 <Button variant="outline-info">Sort</Button>
 
@@ -82,8 +134,61 @@ export default function NotesDisplay() {
               <Button variant="outline-success">Save</Button>{" "}
             </Form>
           </div>
-          <InputNote />
+        
         </Row>
+        <Row>
+       
+              <Card
+        className='position-relative top-0 start-50 translate-middle-x  '
+        style={{
+          width: '50rem',
+        }}
+      >
+        <Button
+          className='position-absolute top-0 start-100 translate-middle rounded-circle p-0 border-0 '
+          variant='primary'
+          style={{ width: '2.5rem' }}
+          onClick={AddNote} 
+        >
+
+          <RiAddLine size='1x' />
+        </Button>
+        <Card.Body>
+          <InputGroup className='mb-3'>
+            <FormControl
+              placeholder='Title'
+              aria-label='Default'
+              aria-describedby='inputGroup-sizing-default'
+              onChange={(e) => setTitle(e.target.value)  }
+            />
+          </InputGroup>
+          <Form.Group className='mb-3' controlId='exampleForm.ControlTextarea1'>
+            <Form.Control placeholder='Content' as='textarea' name='content' rows={3} onChange={(e) => setContent(e.target.value) }  />
+          </Form.Group>
+          <div
+            className='d-flex justify-content-between input-group'
+            style={{ backgroundColor: '#ffff' }}
+          >
+            <input
+              type='file'
+              id='inputGroupFile01'
+              onChange={(e) => setAddImg(e.target.files[0])}
+              aria-describedby='inputGroupFileAddon01'
+            />
+            <button
+              className='input-group-text'
+              id='inputGroupFileAddon01'
+              onClick={uploadImage}
+              disabled={addImg.length === 0 ? true : false}
+              style={{ borderRadius: '5px' }}
+            >
+              Upload
+            </button>
+          </div>
+        </Card.Body>
+      </Card>
+
+          </Row>
         <Row className="d-flex flex-row justify-content-center mt-3">
           <Button
             variant="primary"
@@ -100,13 +205,22 @@ export default function NotesDisplay() {
             Delete
           </Button>
         </Row>
+      
         {/* <Notes data={filteredData} /> */}
-        {userNotes.length > 0 ? (
-          <Notes data={userNotes} />
-        ) : (
-          <p>No results found..</p>
-        )}
+    
+    
       </Container>
+      { userNotes.length > 0 ?filteredData.length>0? (
+          <Notes data={filteredData} />
+        ) : (
+          <Notes data={userNotes} />
+        ):(
+          <p>No results found..</p>
+        )}  
+      
+       
+      {/* {/* { console.log("length",userNotes.length)} */}
     </>
+       
   );
 }
